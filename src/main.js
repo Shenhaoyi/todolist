a770f4b47381a2979d69c41f9d980feb17fb1773
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-15 10:24:19 +0800

    实例化 Vue，挂载 App组件。浏览器不支持 import。

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+
+//创建vue实例，
+new Vue({
+  //挂载app组件
+  el: '#app',
+  components: { //组件名：组件对象
+    App //App:App简写成App
+  },
+  template: '<App/>'
+})
